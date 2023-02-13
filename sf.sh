f7c45bf14411ebd6bb659b1f16dbc365e893b696
jihannova <jihanazzahranova@gmail.com>
2023-02-13 15:02:41 +0700

    update 4.5

--- sf.sh
+++ sf.sh
@@ -3,12 +3,11 @@
 send_maple () {
 cd ~/rom/out/target/product/maple
 
-
-maple_dsds=Nusantara-v5.4-13-maple_dsds-13012023-OFFICIAL-1610.zip
-maple_dsds_gapps=Nusantara-v5.4-13-maple_dsds-Gapps-13012023-OFFICIAL-1536.zip 
-maple=Nusantara-v5.4-13-maple-14012023-OFFICIAL-0219.zip 
-maple_gapps=Nusantara-v5.4-13-maple-Gapps-13012023-OFFICIAL-1420.zip
-project=nusantaraproject/maple  
+#maple_dsds=Nusantara-v5.4-13-maple_dsds-13012023-OFFICIAL-1610.zip
+maple_dsds_gapps=Cherish-OS-v4.5-20230213-0440-maple_dsds-UNOFFICIAL-GApps.zip
+#maple=Nusantara-v5.4-13-maple-14012023-OFFICIAL-0219.zip 
+maple_gapps=Cherish-OS-v4.5-20230213-0601-maple-UNOFFICIAL-GApps.zip
+project=xperia-xz-premium/CherishOS/tiramisu/maple  
 
 # Upload
 expect -c "
@@ -19,19 +18,11 @@ expect \"Password\"
 send \"$SF_PASS\r\"
 set timeout -1
 expect \"sftp>\"
-send \"put $maple\r\"
-expect \"Uploading\"
-expect \"100%\"
-expect \"sftp>\"
 send \"put $maple_gapps\r\"
 expect \"Uploading\"
 expect \"100%\"
 expect \"sftp>\"
 send \"cd ../maple_dsds\r\"
-expect \"sftp>\"
-send \"put $maple_dsds\r\"
-expect \"Uploading\"
-expect \"100%\"
 expect \"sftp>\"
 send \"put $maple_dsds_gapps\r\"
 expect \"Uploading\"
